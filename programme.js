461bee3ec9648d667be0b51807915a14727ac954
Samuel Tomen <113606314+samueltomen@users.noreply.github.com>
2023-06-29 12:29:38 +0200

    exercice algorithme js

--- programme.js
+++ programme.js
@@ -1,1 +1,12 @@
 // Programme 1
+
+let chiffre;
+let chiffre2;
+let resultat;
+
+chiffre = Math.floor(Math.random() * 21 + 1);
+chiffre2 = Math.floor(Math.random() * 21 + 1);
+resultat = chiffre - chiffre2;
+console.log(chiffre);
+console.log(chiffre2);
+console.log(resultat);
